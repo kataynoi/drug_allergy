81d6a7d9c2fde5800999452ff9f749a6923d2200
kataynoi <thait-rex@hotmail.com>
2023-01-26 15:08:49 +0700

    'cidCheck'

--- assets/apps/js/drug_allergy.js
+++ assets/apps/js/drug_allergy.js
@@ -3,15 +3,27 @@ $(document).ready(function () {
   $("#btn_drug_allergy_check").on("click", function () {
     //alert('OK');
     $cid = $("#cid").val();
-    window.location.href = site_url+'/report/drug_allergy/'+$cid ;
+    if (cidCheck($cid)) {
+      window.location.href = site_url + "/report/drug_allergy/" + $cid;
+    }
     exit(0);
   });
 
-  $(document).on('keypress',function(e) {
-    if(e.which == 13) {
-      $cid = $("#cid").val();
-      window.location.href = site_url+'/report/drug_allergy/'+$cid ;
-      exit(0);
+  $(document).on("keypress", function (e) {
+    if (e.which == 13) {
+      $cid = $(this).val();
+      if (cidCheck($cid)) {
+        window.location.href = site_url + "/report/drug_allergy/" + $cid;
+      }
     }
+  });
 });
-});
+
+function cidCheck(cid) {
+  if (cid.length < 13) {
+    swal("เลขบัตรประชาชนไม่ถูกต้อง");
+    return false;
+  } else {
+    return true;
+  }
+}

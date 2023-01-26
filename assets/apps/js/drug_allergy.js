e61d17a5709521b4c0ff96a982ccf887ed0feb79
kataynoi <thait-rex@hotmail.com>
2023-01-26 09:55:35 +0700

    first commit

---
+++ assets/apps/js/drug_allergy.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  var crud = {};
+  $("#btn_drug_allergy_check").on("click", function () {
+    //alert('OK');
+    $cid = $("#cid").val();
+    window.location.href = site_url+'/report/drug_allergy/'+$cid ;
+    exit(0);
+  });
+
+});
